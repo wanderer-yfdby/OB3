d633e25fd37cec8a29bcb26b821f06f012496c34
wanderer-yfdby <gbkfnd@gmail.com>
2024-05-15 15:39:45 +0500

    changes in aggregation

--- aggregation.py
+++ aggregation.py
@@ -6,8 +6,8 @@ class School():
         self.teacher = new_teacher
     def start_lessone(self):
         self.teacher.teach()
-
+# Пример агрегации
 my_teacher = Teacher()
 my_school = School(my_teacher)
-# Пример агрегации
+
 my_school.start_lessone()
